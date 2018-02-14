5a505bcaa6781ea88508e21938f127d62a85ee57
Takashi Ueno <pakkunflower00@gmail.com>
2018-02-15 07:33:36 +0900

    add config

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+
+	"github.com/oneut/lrp/monitor"
+)
+
+func GetConfig() *Config {
+	buf, err := ioutil.ReadFile("lrp.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	config := &Config{}
+	yaml.Unmarshal(buf, config)
+
+	return config
+}
+
+type Config struct {
+	ProxyHost  string `yaml:"proxy_host"`
+	SourceHost string `yaml:"source_host"`
+	Tasks      map[string]monitor.Task
+}
